51c094aaa681f22031761d98a63a5b95d3fad168
xixi <xixi@nodejs.com>
2020-09-20 15:56:14 +0800

    Complete the comment style

--- cloudfunctions/getQrCode/index.js
+++ cloudfunctions/getQrCode/index.js
@@ -10,6 +10,11 @@ exports.main = async(event, context) => {
             scene: wxContext.OPENID,
             // page:扫描进入的页面，但必须等小程序上线之后，才能正常设置跳转
             //获取小程序对应的码时，在对应的页面,js文件中，console.log(options.scene)即可
+            lineColor: {
+                'r': 245,
+                'g': 109,
+                'b': 122
+            }
         })
         // console.log(result);
     const upload = await cloud.uploadFile({
